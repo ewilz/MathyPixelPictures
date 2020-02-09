a28072066ebaf9d088aa498b48c9d49bc99c98ef
Emily Williams <emily.williams@levelk.io>
2020-02-08 19:45:57 -0500

    delete extra functions

--- index.go
+++ index.go
@@ -3,38 +3,11 @@ package main
 import (
     "fmt"
     "syscall/js"
-    "strconv"
     "bytes"
     "encoding/base64"
     "image"
     "image/png"
 )
-
-
-func add(_ js.Value, i []js.Value) interface {} {
-  fmt.Println(i)
-  value1 := js.Global().Get("document").Call("getElementById", i[0].String()).Get("value").String()
-  value2 := js.Global().Get("document").Call("getElementById", i[1].String()).Get("value").String()
-
-  int1, _ := strconv.Atoi(value1)
-  int2, _ := strconv.Atoi(value2)
-
-  js.Global().Get("document").Call("getElementById", i[2].String()).Set("value", int1 + int2)
-  fmt.Println(int1 + int2)
-  return js.ValueOf(int1 + int2)
-}
-
-func subtract(this js.Value, i []js.Value) interface{} {
-  value1 := js.Global().Get("document").Call("getElementById", i[0].String()).Get("value").String()
-  value2 := js.Global().Get("document").Call("getElementById", i[1].String()).Get("value").String()
-
-  int1, _ := strconv.Atoi(value1)
-  int2, _ := strconv.Atoi(value2)
-
-  fmt.Println(int1 - int2)
-  js.Global().Get("document").Call("getElementById", i[2].String()).Set("value", int1 - int2)
-  return js.ValueOf(int1 - int2)
-}
 
 func Pic(dx, dy int) [][]uint8 {
 	// allocate
@@ -63,8 +36,6 @@ func showPic(_ js.Value, i []js.Value) interface{} {
 }
 
 func registerCallbacks() {
-  js.Global().Set("add", js.FuncOf(add))
-  js.Global().Set("subtract", js.FuncOf(subtract))
   js.Global().Set("showPic", js.FuncOf(showPic))
 }
 
